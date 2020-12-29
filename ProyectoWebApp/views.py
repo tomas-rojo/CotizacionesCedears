f36670dcbd4770c9a539523d655412f13c851fd2
tomas-rojo <tomasrojo21@hotmail.com>
2020-12-29 11:13:44 -0300

    Modified style commit

--- ProyectoWebApp/views.py
+++ ProyectoWebApp/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render
 import yfinance as yf
 import datetime
 
+
 # Create your views here.
-
-
 def index(request):
 
     tickers = ['AAPL', 'MELI', 'TSLA', 'AMZN', 'FB', 'GOOG', 'NFLX']
     tickersarg = ['AAPL.BA', 'MELI.BA', 'TSLA.BA', 'AMZN.BA', 'FB.BA', 'GOOGL.BA', 'NFLX.BA']
     ratio = [10, 60, 15, 144, 8, 58, 16]
     datos = []
+    sum_ccl = []
     i = 0
     r = 0
     today = datetime.date.today()
@@ -25,18 +25,22 @@ def index(request):
         ccl = round((dataarg[0] / data[0]) * (ratio[r]), 2)
         y_ccl = round((y_dataarg[0] / y_data[0]) * (ratio[r]), 2)
         variacion = round(ccl-y_ccl, 2)
+        porcentaje = round(((1-(y_ccl/ccl))*100), 2)
+        sum_ccl.append(ccl)
+        total = sum(sum_ccl)
+        average_ccl = round((total / 7), 2)
 
         info = {
             'tickers': tickers[i],
             'ccl': ccl,
-            'variacion': variacion
+            'variacion': variacion,
+            'porcentaje': porcentaje,
         }
         i += 1
         r += 1
         datos.append(info)
-        print(ccl)
 
     context = {'datos': datos}
-    #print(context)
+    print(average_ccl)
 
     return render(request, 'root/index.html', context)
